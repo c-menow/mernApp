8fac1592065518d1a9f5b3c020a91f6bfed3c4df
Clara C <clarafgf@gmail.com>
2018-10-13 12:45:44 +0200

    Passport JWT authentication strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-   mongoURI: 'mongodb://<user>:<password>@ds235302.mlab.com:35302/curso_mern_devconnector'
+   mongoURI: 'mongodb://<user>:<password>@ds235302.mlab.com:35302/curso_mern_devconnector',
+   secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load User model:
 const User = require('../../models/User');
@@ -71,12 +74,41 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
       .then( isMatch => {
          if(isMatch){   
-            res.json({msg: 'Success'});
+            // User matched - create JWT Payload:
+            const payload = {
+               id: user.id,
+               name: user.name,
+               avatar: user.avatar
+            }
+            // Sign Token
+            jwt.sign(
+               payload,
+               keys.secretOrKey,
+               { expiresIn: 3600 },
+               (err,token) => {
+                  res.json({
+                     success: true,
+                     token: 'Bearer ' + token
+                  })
+               });
          } else {
             return res.status(400).json({password: 'Incorrect password'})
          }
       })
    })
+});
+
+
+// @route  GET api/users/current
+// @desc   Return current user
+// @access  Private (it is unathorized without the token)
+router.get('/current', 
+   passport.authenticate('jwt', {session: false}), (req, res) => {
+   res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+   });
 });
 
 
